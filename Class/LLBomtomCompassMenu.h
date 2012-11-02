a713951f40310d52c9480d4aa35fed09d238bb16
lequiet <ganxiangle@gmail.com>
2012-11-03 00:31:18 +0800

    add a files

---
+++ Class/LLBomtomCompassMenu.h
@@ -0,0 +1,67 @@
+//
+//  LLBomtomCompassMenu.h
+//
+//  Created by Xiangle le on 12-11-2.
+//  Copyright (c) 2012年 ganxiangle@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#define LLBOMTOMCOMPASSMENU_VIEW_HEIGHT 135.0
+#define LLBOMTOMCOMPASSMENU_VIEW_WIDTH  296.0
+
+typedef enum {
+    kLLBomtomCompassMenuCenterBtnTag = 100,
+    kLLBomtomCompassMenuInnerBtnTag_1 = 1,
+    kLLBomtomCompassMenuInnerBtnTag_2 = 2,
+    kLLBomtomCompassMenuInnerBtnTag_3 = 3, 
+    kLLBomtomCompassMenuInnerBtnTag_4 = 4,
+    kLLBomtomCompassMenuInnerBtnTag_5 = 5,
+    kLLBomtomCompassMenuOutterBtnTag_1 = 11,
+    kLLBomtomCompassMenuOutterBtnTag_2 = 12,
+    kLLBomtomCompassMenuOutterBtnTag_3 = 13,
+    kLLBomtomCompassMenuOutterBtnTag_4 = 14,
+    kLLBomtomCompassMenuOutterBtnTag_5 = 15,
+    kLLBomtomCompassMenuOutterBtnTag_6 = 16,
+    kLLBomtomCompassMenuOutterBtnTag_7 = 17
+}LLBomtomCompassMenuButtonTag;
+
+@class LLBomtomCompassMenu;
+@protocol LLBomtomCompassMenuDelegate <NSObject>
+
+@optional
+- (void)LLBomtomCompassMenu:(LLBomtomCompassMenu *)menu buttonClickedWithTag:(LLBomtomCompassMenuButtonTag)tag;
+- (void)LLBomtomCompassMenuWillHide:(LLBomtomCompassMenu *)menu;
+- (void)LLBomtomCompassMenuWillShow:(LLBomtomCompassMenu *)menu;
+- (void)LLBomtomCompassMenuDidHide:(LLBomtomCompassMenu *)menu;
+- (void)LLBomtomCompassMenuDidShow:(LLBomtomCompassMenu *)menu;
+- (void)LLBomtomCompassMenuWillShowRotationAnimation:(LLBomtomCompassMenu *)menu;
+- (void)LLBomtomCompassMenuWillHideRotationAnimation:(LLBomtomCompassMenu *)menu;
+- (void)LLBomtomCompassMenuDidShowRotationAnimation:(LLBomtomCompassMenu *)menu;
+- (void)LLBomtomCompassMenuDidHideRotationAnimation:(LLBomtomCompassMenu *)menu;
+@end
+
+
+@interface LLBomtomCompassMenu : UIView
+{
+    UIImageView *_rotationView;
+    UIImageView *_innerView;
+    BOOL _isRotationViewShow;//NO 为不显示状态 
+    BOOL _isMenuShow;
+    id<LLBomtomCompassMenuDelegate> delegate;
+}
+
+@property (nonatomic, retain)  id<LLBomtomCompassMenuDelegate> delegate;
+
+- (BOOL)isMenuShow;
+- (void)showOrHideMenu;
+- (void)addButtonToCenterWithImage:(UIImage *)image highlightedImage:(UIImage *)highlightedImage;
+- (void)addButtonsToFirstRoundWithImages:(NSArray *)images 
+                       highlightedImages:(NSArray *)highlightedImages;
+- (void)addButtonsToFirstRoundWithImages:(NSArray *)images;
+- (void)addButtonsToSecondRoundWithImages:(NSArray *)images highlightedImages:(NSArray *)highlightedImages;
+- (void)addButtonsToSecondRoundWithImages:(NSArray *)images;
+
+@end
+
---
+++ Class/LLBomtomCompassMenu.m
@@ -0,0 +1,285 @@
+//
+//  LLBomtomCompassMenu.m
+//
+//  Created by ganxiangle on 12-3-12.
+//  Copyright (c) 2012年 ganxiangle@gmail.com. All rights reserved.
+//
+
+#import "LLBomtomCompassMenu.h"
+#import <QuartzCore/QuartzCore.h>
+
+#define DEGREES_TO_RADIANS(d) (d * M_PI / 180)
+#define VIEW_HEIGHT 135.0
+#define VIEW_WEIGHT 296.0
+
+
+@implementation LLBomtomCompassMenu
+@synthesize delegate;
+
+- (id)init
+{
+    self = [self initWithFrame: CGRectMake(0, 0, VIEW_WEIGHT, VIEW_HEIGHT)];
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) 
+    {
+        _isRotationViewShow = YES;
+        _isMenuShow = YES;
+        [self initView];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [_innerView release];
+    [_rotationView release];
+    [super dealloc];
+}
+
+- (void)initView
+{
+
+    // The top large image view
+    _rotationView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"menu_large"]];
+    _rotationView.frame = CGRectMake(0.0, VIEW_HEIGHT/2.0, VIEW_WEIGHT, VIEW_HEIGHT);   
+    _rotationView.userInteractionEnabled = YES;
+    _rotationView.layer.anchorPoint = CGPointMake(0.5, 1.0);
+    [_rotationView setUserInteractionEnabled:YES];
+    [self addSubview:_rotationView];
+    
+    // The inner image view
+    _innerView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"menu_inner"]];
+    _innerView.frame = CGRectMake(53.0, 49.0, 191.0, 86.0);
+    [_innerView setUserInteractionEnabled:YES];
+    [self addSubview:_innerView];
+    
+}
+
+- (CGRect)getShowFrame
+{
+    return CGRectMake( (320.0 - VIEW_WEIGHT)/2.0, 460.0 - VIEW_HEIGHT + 14, VIEW_WEIGHT, VIEW_HEIGHT);
+}
+
+- (CGRect)getHideFrame
+{
+    return CGRectMake( (320.0 - VIEW_WEIGHT)/2.0, 460.0, VIEW_WEIGHT, VIEW_HEIGHT);
+}
+
+- (void)addButtonsToFirstRoundWithImages:(NSArray *)images 
+                       highlightedImages:(NSArray *)highlightedImages
+{
+    CGRect rects[5];
+    rects[0] = CGRectMake(13, 55,  25, 25);
+    rects[1] = CGRectMake(37, 22,  25, 25);
+    rects[2] = CGRectMake(83, 6,   25, 25);
+    rects[3] = CGRectMake(130, 22, 25, 25);
+    rects[4] = CGRectMake(154, 55, 25, 25);    
+    
+    for (NSInteger i = 0; i < [images count] && i < 5; i ++) {
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = rects[i];
+        [btn setShowsTouchWhenHighlighted:YES];
+        btn.tag = kLLBomtomCompassMenuInnerBtnTag_1 + i;
+        [btn addTarget:self action:@selector(menuButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+        
+        if ([[images objectAtIndex:i] isKindOfClass:[UIImage class]]) {
+            [btn setImage:[images objectAtIndex:i] forState:UIControlStateNormal];
+            [_innerView addSubview:btn];
+            if (highlightedImages && [[highlightedImages objectAtIndex:i] isKindOfClass:[UIImage class]]) {
+                [btn setImage:[highlightedImages objectAtIndex:i] forState:UIControlStateHighlighted];
+            }
+        }
+    }
+}
+
+- (void)addButtonsToFirstRoundWithImages:(NSArray *)images
+{
+    [self addButtonsToFirstRoundWithImages:images highlightedImages:nil];
+}
+
+- (void)addButtonsToSecondRoundWithImages:(NSArray *)images highlightedImages:(NSArray *)highlightedImages
+{
+    CGRect rects[7];
+    rects[0] = CGRectMake(14, 99,  25, 25);
+    rects[1] = CGRectMake(39, 55,  25, 25);
+    rects[2] = CGRectMake(80, 24,  25, 25);
+    rects[3] = CGRectMake(136, 11, 25, 25);
+    rects[4] = CGRectMake(191, 24, 25, 25);
+    rects[5] = CGRectMake(231, 55, 25, 25);
+    rects[6] = CGRectMake(254, 99, 25, 25);
+
+    for (NSInteger i = 0; i < [images count] && i < 7; i ++) {
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = rects[i];
+        [btn setShowsTouchWhenHighlighted:YES];
+        btn.tag = kLLBomtomCompassMenuOutterBtnTag_1 + i;
+        [btn addTarget:self 
+                action:@selector(menuButtonClicked:) 
+      forControlEvents:UIControlEventTouchUpInside];
+        
+        if ([[images objectAtIndex:i] isKindOfClass:[UIImage class]]) {
+            [btn setImage:[images objectAtIndex:i] forState:UIControlStateNormal];
+            [_rotationView addSubview:btn];
+            
+            if (highlightedImages && [[highlightedImages objectAtIndex:i] isKindOfClass:[UIImage class]]) {
+                [btn setImage:[highlightedImages objectAtIndex:i] forState:UIControlStateHighlighted];
+            }
+        }
+    }
+}
+
+- (void)addButtonsToSecondRoundWithImages:(NSArray *)images 
+{
+    [self addButtonsToSecondRoundWithImages:images highlightedImages:nil];
+}
+
+- (void)addButtonToCenterWithImage:(UIImage *)image highlightedImage:(UIImage *)highlightedImage
+{
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(83, 55, 25, 25);
+    [btn setShowsTouchWhenHighlighted:YES];
+    btn.tag = kLLBomtomCompassMenuCenterBtnTag;
+    [btn addTarget:self 
+            action:@selector(menuButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    if (image) {
+        [btn setImage:image forState:UIControlStateNormal];
+        [_innerView addSubview:btn];
+        
+        if (highlightedImage) {
+            [btn setImage:highlightedImage forState:UIControlStateHighlighted];
+        }
+    }    
+}
+
+- (void)menuButtonClicked:(id)sender
+{
+    UIButton *button = (UIButton *)sender;
+    /* when the center button clicked */
+    if (button.tag == kLLBomtomCompassMenuCenterBtnTag) {
+        button.userInteractionEnabled = NO;
+        [UIView beginAnimations:@"present-countdown" context:nil];
+        [UIView setAnimationDuration:1];
+        [UIView setAnimationDelegate:self];
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+        [UIView setAnimationDidStopSelector:@selector(rotationAnimationDidStop)];
+
+        CGFloat angle = _isRotationViewShow ? 180.0 : 0.0;
+        _rotationView.layer.transform = CATransform3DMakeRotation(DEGREES_TO_RADIANS(angle), 
+                                                                 0.0, 0.0, 1.0);
+        
+        [self rotationAnimationWillStart];
+        [UIView commitAnimations];
+    } 
+
+
+    if (self.delegate && [self.delegate respondsToSelector:@selector(dd)]) {
+        [self.delegate LLBomtomCompassMenu:self buttonClickedWithTag:button.tag];
+    }
+    
+    NSLog(@"menu tag %d clicked", button.tag);
+}
+
+- (void)rotationAnimationWillStart
+{
+    if (_isRotationViewShow && 
+        self.delegate && 
+        [self.delegate respondsToSelector:@selector(LLBomtomCompassMenuWillHideRotationAnimation:)]) 
+    {        
+        [self.delegate LLBomtomCompassMenuWillHideRotationAnimation:self];
+        
+    } else if (!_isRotationViewShow && 
+               self.delegate && 
+               [self.delegate respondsToSelector:@selector(LLBomtomCompassMenuWillShowRotationAnimation:)] ) 
+    {        
+        [self.delegate LLBomtomCompassMenuWillShowRotationAnimation:self];
+    }
+}
+
+- (void)rotationAnimationDidStop
+{
+    UIButton *menuButton =  (UIButton *)[self viewWithTag:kLLBomtomCompassMenuCenterBtnTag];
+    menuButton.userInteractionEnabled = YES;
+    
+    if (_isRotationViewShow && 
+        self.delegate && 
+        [self.delegate respondsToSelector:@selector(LLBomtomCompassMenuDidHideRotationAnimation:)]) 
+    {        
+        [self.delegate LLBomtomCompassMenuDidHideRotationAnimation:self];
+        
+    } else if (!_isRotationViewShow && 
+               self.delegate && 
+               [self.delegate respondsToSelector:@selector(LLBomtomCompassMenuDidShowRotationAnimation:)] ) 
+    {        
+        [self.delegate LLBomtomCompassMenuDidShowRotationAnimation:self];
+    }
+    
+    
+    _isRotationViewShow = !_isRotationViewShow;
+}
+
+- (void)showOrHideMenu
+{
+    [UIView beginAnimations:@"present-countdown" context:nil];
+    [UIView setAnimationDuration:1];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(showOrHideMenuAnimationDidStop)];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    
+    if (_isMenuShow) 
+    {
+        self.frame = [self getHideFrame];
+    }
+    else
+    {
+        self.frame = [self getShowFrame];
+    }
+    [self showOrHideMenuAnimationWillStart];
+    [UIView commitAnimations];
+}
+
+- (void)showOrHideMenuAnimationWillStart
+{
+    if (_isMenuShow && 
+        self.delegate && 
+        [self.delegate respondsToSelector:@selector(LLBomtomCompassMenuWillHide:)]) {   
+        
+        [self.delegate LLBomtomCompassMenuWillHide:self];
+        
+    } else if (!_isMenuShow && 
+               self.delegate && 
+               [self.delegate respondsToSelector:@selector(LLBomtomCompassMenuWillShow:)]) {
+        [self.delegate LLBomtomCompassMenuWillShow:self];
+    }
+}
+
+- (void)showOrHideMenuAnimationDidStop
+{
+    
+    if (_isMenuShow && 
+        self.delegate && 
+        [self.delegate respondsToSelector:@selector(LLBomtomCompassMenuDidHide:)]) {   
+        
+        [self.delegate LLBomtomCompassMenuDidHide:self];
+        
+    } else if (!_isMenuShow && 
+               self.delegate && 
+               [self.delegate respondsToSelector:@selector(LLBomtomCompassMenuDidShow:)]) {
+        [self.delegate LLBomtomCompassMenuDidShow:self];
+    }
+    
+    _isMenuShow = !_isMenuShow;
+}
+
+- (BOOL)isMenuShow
+{
+    return _isMenuShow;
+}
+
+@end
+
