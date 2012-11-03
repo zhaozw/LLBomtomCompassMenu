6bf96180c68c6f3eff8152ff2c591f7f24c1741c
lequiet <ganxiangle@gmail.com>
2012-11-03 11:30:44 +0800

    change the dir system

---
+++ LLBomtomCompassMenu/LLBomtomCompassMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LLBomtomCompassMenu
+//
+//  Created by Xiangle le on 12-11-3.
+//  Copyright (c) 2012年 GUET/Wondershare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LLAppDelegate class]));
+    }
+}
