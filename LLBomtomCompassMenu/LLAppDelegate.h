3d7f58ab79ec71256ffb4f0e37f218eaf470a6f9
lequiet <ganxiangle@gmail.com>
2012-11-03 11:30:14 +0800

    add some files

---
+++ LLBomtomCompassMenu/LLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LLAppDelegate.h
+//  LLBomtomCompassMenu
+//
+//  Created by Xiangle le on 12-11-3.
+//  Copyright (c) 2012年 GUET/Wondershare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
